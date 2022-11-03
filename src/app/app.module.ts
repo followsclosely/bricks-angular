10e9bb128ec9d471874b1602def8cb303fcb6e91
followsclosely <mlavwilson@gmail.com>
2022-11-03 10:10:11 -0400

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,25 @@ import { HelloComponent } from './hello.component';
 
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatSelectModule} from '@angular/material/select';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
 
 import { FranchiseService } from './franchise.service';
 
 @NgModule({
-  imports:      [ HttpClientModule, BrowserModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule, MatCheckboxModule, MatSelectModule ],
+  imports:      [ 
+    HttpClientModule, 
+    BrowserModule, 
+    BrowserAnimationsModule, 
+    FormsModule, 
+    ReactiveFormsModule, 
+    MatCheckboxModule, 
+    MatSelectModule, 
+    MatButtonToggleModule,
+    MatButtonModule,
+    MatTabsModule
+   ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ],
   providers:    [ FranchiseService ]
